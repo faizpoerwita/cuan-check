f53e89a7ba7e9b849ae44322074b0095262b4a96
faizpoerwita <ilmuwankecil@gmail.com>
2024-12-18 07:23:53 +0700

    fix: Remove unused dependencies from Vite manual chunks configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,7 +11,6 @@ export default defineConfig({
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', 'react-router-dom'],
-          ui: ['@headlessui/react', '@heroicons/react'],
           chart: ['chart.js', 'react-chartjs-2']
         }
       }
